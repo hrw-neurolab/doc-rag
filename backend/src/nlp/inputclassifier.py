fc801471796be221925cac328e1401b5131dd4a3
doyouwantsometea <e920542@gmail.com>
2025-10-07 11:19:39 +0200

    Revert "replace llm query classifier with roberta"
    This reverts commit 194b088cab4e0fa6c9cd9402111c134d7f33dfe1.

--- backend/src/nlp/inputclassifier.py
+++ backend/src/nlp/inputclassifier.py
@@ -1,61 +1,50 @@
 import asyncio
 from langdetect import detect
-# from langchain_ollama import ChatOllama
-# from langchain_core.prompts import (
-#     ChatPromptTemplate,
-#     HumanMessagePromptTemplate,
-#     SystemMessagePromptTemplate,
-# )
-# from langchain_core.output_parsers import StrOutputParser
-from transformers import pipeline
-# from src.nlp.clients import CLASSIFIER_CLIENT
-from src.config import CONFIG
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
+from langchain_core.output_parsers import StrOutputParser
+
+from src.nlp.clients import CLASSIFIER_CLIENT
 
 
-# system_message = """\
-# You are a helpful assistant that decides whether a query is knowledge-intensive and requires looking up resources.
-# Except for simple chit-chats or short responses (e.g., "how are you","thank you", "good to know", "this is interesting"), most questions should fell into the category "requires resources" by default, and thereby a True as output.
-# Return a clean boolean value True or False. Do NOT append any additional elaboration or explanation.\
-# """
+system_message = """\
+You are a helpful assistant that decides whether a query is knowledge-intensive and requires looking up resources.
+Except for simple chit-chats or short responses (e.g., "how are you","thank you", "good to know", "this is interesting"), most questions should fell into the category "requires resources" by default, and thereby a True as output.
+Return a clean boolean value True or False. Do NOT append any additional elaboration or explanation.\
+"""
 
-# human_message = """\
-# Here is the query:
+human_message = """\
+Here is the query:
 
-# {query}\
-# """
-
-
-# CLASSIFIER_PROMPT_TEMPLATE = ChatPromptTemplate(
-#     [
-#         SystemMessagePromptTemplate.from_template(system_message),
-#         HumanMessagePromptTemplate.from_template(human_message),
-#     ]
-# )
+{query}\
+"""
 
 
-# chain = CLASSIFIER_PROMPT_TEMPLATE | CLASSIFIER_CLIENT | StrOutputParser()
+CLASSIFIER_PROMPT_TEMPLATE = ChatPromptTemplate(
+    [
+        SystemMessagePromptTemplate.from_template(system_message),
+        HumanMessagePromptTemplate.from_template(human_message),
+    ]
+)
 
-model_dir = "backend/src/nlp"
-model_name = CONFIG.input_classifier.model_name.split("/")[-1]
-pipeline = pipeline(task="text-classification", model=f"{model_dir}/{model_name}")
+
+chain = CLASSIFIER_PROMPT_TEMPLATE | CLASSIFIER_CLIENT | StrOutputParser()
+
 
 
 async def lang_check(query: str = "") -> str:
     language = "de" if detect(query) == "de" else "en"
     return language
 
-# async def retrieve_check(query: str = "", timeout_s: float = 3.0) -> bool:
-#     prompt_vars = {"query": query}
-#     try:
-#         resp = await asyncio.wait_for(chain.ainvoke(prompt_vars), timeout=timeout_s)
-#     except asyncio.TimeoutError:
-#         return True  # safe fallback: retrieve
-#     out = (resp or "").strip().split()[0].lower()
-#     return out == "true"
-
-async def retrieve_check(query: str = "") -> bool:
-    if not query:
-        return False
-    
-    prediction = pipeline(query)
-    return True if prediction[0]["label"] == "LABEL_1" else False
+async def retrieve_check(query: str = "", timeout_s: float = 3.0) -> bool:
+    prompt_vars = {"query": query}
+    try:
+        resp = await asyncio.wait_for(chain.ainvoke(prompt_vars), timeout=timeout_s)
+    except asyncio.TimeoutError:
+        return True  # safe fallback: retrieve
+    out = (resp or "").strip().split()[0].lower()
+    return out == "true"
