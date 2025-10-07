9db65d31cad29fbab54ef4a0f8e580cd171acbf6
doyouwantsometea <e920542@gmail.com>
2025-10-07 11:19:39 +0200

    Revert "hf repo path"
    This reverts commit 356ccbc6662433649c84a8fa0f4260c6286bde75.

--- backend/src/nlp/inputclassifier.py
+++ backend/src/nlp/inputclassifier.py
@@ -49,13 +49,7 @@ from src.config import CONFIG
 # model_path = Path(model_dir) / model_name
 
 
-# from pathlib import Path
-
-model_dir = Path("backend/src/nlp") / CONFIG.input_classifier.model_name.split("/")[-1]
-assert model_dir.exists(), f"Model path does not exist: {model_dir}"
-
-
-# model_dir = CONFIG.input_classifier.model_name.split("/")[-1]
+model_dir = CONFIG.input_classifier.model_name.split("/")[-1]
 
 # Load manually
 tokenizer = AutoTokenizer.from_pretrained(model_dir, local_files_only=True)
